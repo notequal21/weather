0a386d59915df29dfe8d6f48057a5e47833c0132
notequal21 <kalorigen@gmail.com>
2022-01-10 15:41:32 +0300

    UI was updated

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,3 +1,4 @@
+import { Divider } from "antd"
 import style from "./Weather.module.scss"
 
 let Weather = () => {
@@ -5,8 +6,55 @@ let Weather = () => {
     <>
       <div className={`${style.weather}`}>
         <div className="container">
+          <div className={`${style.weather__city}`}>
+            Your current location: Moscow
+          </div>
+          <Divider />
           <div className={`${style.weatherBody}`}>
-            Здесь будет показыаться погода
+            <div className={`${style.weatherBody__current}`}>
+              <div className={`${style.weatherBody__title}`}>
+                Current weather
+              </div>
+              <Divider />
+            </div>
+            <div className={`${style.weatherBody__days}`}>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Tuesday
+                </div>
+                <Divider />
+              </div>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Wednesday
+                </div>
+                <Divider />
+              </div>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Thursday
+                </div>
+                <Divider />
+              </div>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Friday
+                </div>
+                <Divider />
+              </div>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Saturday
+                </div>
+                <Divider />
+              </div>
+              <div className={`${style.weatherBody__daysItem}`}>
+                <div className={`${style.weatherBody__title}`}>
+                  Sunday
+                </div>
+                <Divider />
+              </div>
+            </div>
           </div>
         </div>
       </div>
