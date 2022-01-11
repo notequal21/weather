3568351db19c91093580bf05e12e8eed8410e060
notequal21 <kalorigen@gmail.com>
2022-01-11 18:36:08 +0300

    footer was updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import style from "./Footer.module.scss"
+import logoIco from '../../assets/icons/sun.png'
 
 let Footer = () => {
   return (
     <>
       <div className={`${style.footer}`}>
-        <div className="container">
+        <div className={`container`}>
           <div className={`${style.footerBody}`}>
-            здесь будет контактная информация
+            <div className={`${style.footerBody__logo}`}>
+              <img src={logoIco} alt="" /> Weather App
+            </div>
           </div>
         </div>
       </div>
