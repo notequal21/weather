1925896409ba3fc40c3f7f0a3d4bfb57d62cb845
notequal21 <kalorigen@gmail.com>
2022-01-11 16:19:11 +0300

    func for day of week returning

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -2,6 +2,7 @@ import { Divider } from "antd"
 import style from "./Weather.module.scss"
 import axios from 'axios';
 import store from '../../store/store'
+import { useState } from "react";
 
 export const getWeatherInfo = () => {
   const instance = axios.create({
@@ -36,41 +37,12 @@ export const getWeatherInfo = () => {
 
 getWeatherInfo()
 
-// const instance = axios.create({
-//   baseURL: 'https://api.openweathermap.org/data/2.5/'
-// })
-
-// instance.get(`weather?q=${store.location}&appid=83d9c12644f83eb94f3ad2d262c259e2&units=metric&lang=ru`)
-//   .then(response => {
-//     store.currentLocation = response.data.name
-//     store.coord.lon = response.data.coord.lon
-//     store.coord.lat = response.data.coord.lat
-//     store.weather.icon = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-//     store.weather.desc = response.data.weather[0].description
-//     store.weather.temp.current = Math.round(response.data.main.temp)
-//     store.weather.temp.feelsLike = Math.round(response.data.main.feels_like)
-//     store.weather.temp.min = Math.round(response.data.main.temp_max)
-//     store.weather.temp.max = Math.round(response.data.main.temp_min)
-//   })
-//   .then(() => {
-//     instance.get(`onecall?lat=${store.coord.lat}&lon=${store.coord.lon}&appid=83d9c12644f83eb94f3ad2d262c259e2&units=metric&lang=ru`)
-//       .then(response => {
-//         store.days.forEach((item, index) => {
-//           item.temp.day = Math.round(response.data.daily[index].temp.day)
-//           item.temp.feelsLike.day = Math.round(response.data.daily[index].feels_like.day)
-//           item.date.day = new Date(response.data.daily[index].dt * 1000).getDay()
-//           item.date.current = new Date(response.data.daily[index].dt * 1000)
-//             .toLocaleString().split(',')[0]
-//         })
-//       })
-//   })
-
 let CurrentWeather = (props) => {
   return (
     <>
       <div className={`${style.weatherBody__current}`}>
         <div className={`${style.weatherBody__title}`}>
-          Current weather
+          Текущая погода
         </div>
         <div className={`${style.weatherBody__ico}`}>
           <img src={store.weather.icon} alt="weather ico" />
@@ -78,19 +50,13 @@ let CurrentWeather = (props) => {
         <Divider />
         <div className={`${style.weatherItem__content}`}>
           <div className={`${style.weatherItem__contentItem}`}>
-            Desc: {store.weather.desc}
+            Температура {store.weather.temp.current}°
           </div>
           <div className={`${style.weatherItem__contentItem}`}>
-            Temp: {store.weather.temp.current}°
+            Ощущается как {store.weather.temp.feelsLike}°
           </div>
           <div className={`${style.weatherItem__contentItem}`}>
-            Feels like: {store.weather.temp.feelsLike}°
-          </div>
-          <div className={`${style.weatherItem__contentItem}`}>
-            TempMin: {store.weather.temp.min}°
-          </div>
-          <div className={`${style.weatherItem__contentItem}`}>
-            TempMax: {store.weather.temp.max}°
+            {store.weather.desc}
           </div>
           <Divider />
           <div className={`${style.weatherItem__contentItem}`}>
@@ -106,11 +72,34 @@ let CurrentWeather = (props) => {
 }
 
 let WeatherDay = (props) => {
+
+  let currentDay = props.day
+
+  let setCurrentDay = () => {
+    if (currentDay === 0) {
+      currentDay = 'Воскресенье'
+    } else if (currentDay === 1) {
+      currentDay = 'Понедельник'
+    } else if (currentDay === 2) {
+      currentDay = 'Вторник'
+    } else if (currentDay === 3) {
+      currentDay = 'Среда'
+    } else if (currentDay === 4) {
+      currentDay = 'Четверг'
+    } else if (currentDay === 5) {
+      currentDay = 'Пятница'
+    } else if (currentDay === 6) {
+      currentDay = 'Суббота'
+    }
+  }
+
+  setCurrentDay()
+
   return (
     <>
       <div className={`${style.weatherBody__daysItem}`}>
         <div className={`${style.weatherBody__title}`}>
-          {props.day}
+          {currentDay}
           <br />
           {props.date}
         </div>
@@ -143,7 +132,7 @@ let Weather = () => {
         <div className="container">
 
           <div className={`${style.weather__city}`}>
-            Your current location: {store.currentLocation}
+            Ваше местоположение: {store.currentLocation}
           </div>
           <Divider />
           <div className={`${style.weatherBody}`}>
