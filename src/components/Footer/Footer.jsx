c26a92cfa035a134d2ecffb757e8adba2c964ad8
notequal21 <kalorigen@gmail.com>
2022-01-11 19:00:02 +0300

    socials in footer was updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,9 @@ import logoIco from '../../assets/icons/sun.png'
 import telegramIco from '../../assets/icons/telegram.png'
 import githubIco from '../../assets/icons/github.png'
 import resumeIco from '../../assets/icons/resume.png'
+import gmailIco from '../../assets/icons/gmail.png'
+import linkedinIco from '../../assets/icons/linkedin.png'
+import portfolioIco from '../../assets/icons/portfolio.png'
 
 let Footer = () => {
   return (
@@ -20,13 +23,28 @@ let Footer = () => {
                 </a>
               </div>
               <div className={`${style.footerBody__socialsItem}`}>
+                <a target={`_blank`} href="https://www.linkedin.com/in/kirill-makhnev-9344a8226/">
+                  <img src={linkedinIco} alt="" />
+                </a>
+              </div>
+              <div className={`${style.footerBody__socialsItem}`}>
                 <a target={`_blank`} href="https://github.com/notequal21">
                   <img src={githubIco} alt="" />
                 </a>
               </div>
               <div className={`${style.footerBody__socialsItem}`}>
+                <a target={`_blank`} href="mailto:kir.mkhnv@gmail.com">
+                  <img src={gmailIco} alt="" />
+                </a>
+              </div>
+              <div className={`${style.footerBody__socialsItem}`}>
                 <a target={`_blank`} href="https://notequal.notion.site/5563732749bd4087ba5441b23f684a08">
                   <img src={resumeIco} alt="" />
+                </a>
+              </div>
+              <div className={`${style.footerBody__socialsItem}`}>
+                <a target={`_blank`} href="https://notequal21.github.io/portfolio/">
+                  <img src={portfolioIco} alt="" />
                 </a>
               </div>
             </div>
