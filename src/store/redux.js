ce39848dfe28e40d6ed1bd58e827ea5efd6a2670
notequal21 <kalorigen@gmail.com>
2022-01-19 11:48:25 +0300

    added redux-thunk
    installed redux-thunk
    installed redux-extenssion
    added middleware

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import currentWeatherReducer from "./reducers/currentWeatherReducer";
 import weatherDaysOfWeekReducer from "./reducers/weatherDaysOfWeekReducer";
 
@@ -7,6 +9,6 @@ let rootReducer = combineReducers({
   weatherDaysOfWeek: weatherDaysOfWeekReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
